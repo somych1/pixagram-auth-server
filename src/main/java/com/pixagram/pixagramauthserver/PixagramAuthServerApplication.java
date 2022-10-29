2ee9b57bd9c37d2fbb41ea7cb824455b7d346a7b
samat <somych1@gmail.com>
2022-10-28 22:03:05 -0500

    first commit

---
+++ src/main/java/com/pixagram/pixagramauthserver/PixagramAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.pixagram.pixagramauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixagramAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixagramAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixagram/pixagramauthserver/PixagramAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixagram.pixagramauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixagramAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
